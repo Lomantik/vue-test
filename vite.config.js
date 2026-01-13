067de8462d1d2b61c1303c74b31aebcb133c9a99
Lomantik <lomantikus@gmail.com>
2026-01-13 03:57:38 +0300

    Build fix for Pages

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,16 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const isGithubPages = mode === 'github-pages'
+
+  return {
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  base: '/vue-test/',
+    base: isGithubPages ? '/vue-test/' : '/',
+  }
 })
